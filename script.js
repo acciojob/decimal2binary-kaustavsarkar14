7a2cfab5990581e789f65276021d9e9d60fb05cf
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-09 12:04:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  let ans = ""
+	while(num>0){
+		if(num%2===0)
+			ans+=0
+    else 
+        ans+=1
+      num = Math.floor(num / 2)
+	}
+  return ans.split("").reverse().join("")
 }
 
 window.decimalToBinary = decimalToBinary;
